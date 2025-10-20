9e482dd6e2e742fb8435fea305e9ced00790254f
Enmanuel D Becerra C <becerracenmanueld@gmail.com>
2025-10-20 19:11:13 -0400

    update

--- compile.sh
+++ compile.sh
@@ -1,5 +1,13 @@
+export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
+
 #WINDOWS
-#time g++ -o main main.cpp -I ./include -L ./lib -lraylib -lssl -lcrypto ; ./main
+#time g++ -o main main.cpp -I ./include -L ./lib -lraylib -lssl -lcrypto
 
 #LINUX
-time g++ -o main main.cpp -I./include -L./lib -lraylib -lssl -lcrypto ; ./main
+time g++ -o main main.cpp -I./include -L./lib -lraylib -lssl -lcrypto
+
+if [ ! $? -eq 0 ]; then
+    echo "exit error"; exit;
+fi
+
+./main
